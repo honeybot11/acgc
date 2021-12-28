08260ab5e4dcdc9b6388b982411708a2afd72610
Ss <88151915+honeybot11@users.noreply.github.com>
2021-12-28 13:04:13 +0530

    Update new.py

--- new.py
+++ new.py
@@ -4,6 +4,5 @@ deviceid="32D3085F471DF87A00FB4CE43052685FE93239644F24F4E6E9B3D4EEBF0C4C764F0B76
 emaill="drkipt7m@xojxe.com"
 passwordd="test11"
 custompwd="ixd123"
-url="https://ElatedSlipperyArchive.crimsondead.repl.co"
 chatlink="http://aminoapps.com/p/3s2pzw"
 nickname="loveixd"
