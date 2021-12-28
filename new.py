08b453269aed22dfd892a5405f4c7adc25e71cf3
Ss <88151915+honeybot11@users.noreply.github.com>
2021-12-28 17:27:17 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,5 +1,5 @@
 key='b657ee53-37fb-4805-b19b-8158b4f108ce'
-app_name="acbo"
+app_name="moonlov"
 deviceid="32859B9E59DE6B62DDA7DA83DB2DACB02BD4C2EF7D192A38E05E03575C32A9563CFBBCB9F713F4DF69"
 emaill="tuhsqye3@1secmail.net"
 passwordd="ixd123"
