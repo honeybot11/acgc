9220db14343a6b6cbab6f03862728192764479fe
Ss <88151915+honeybot11@users.noreply.github.com>
2021-12-28 12:41:02 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,5 +1,5 @@
 key='8ae09f0e-4d41-45cf-988c-545e62fe4aac'
-app_name="accgen123"
+app_name="acbo"
 deviceid="32D3085F471DF87A00FB4CE43052685FE93239644F24F4E6E9B3D4EEBF0C4C764F0B76788481A7F955"
 emaill="drkipt7m@xojxe.com"
 passwordd="test11"
