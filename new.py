c60ab75976fc52da96db6a72567c5b14c7f73236
Ss <88151915+honeybot11@users.noreply.github.com>
2021-12-28 17:17:55 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,9 +1,9 @@
 key='b657ee53-37fb-4805-b19b-8158b4f108ce'
 app_name="acbo"
-deviceid="32D3085F471DF87A00FB4CE43052685FE93239644F24F4E6E9B3D4EEBF0C4C764F0B76788481A7F955"
-emaill="drkipt7m@xojxe.com"
-passwordd="test11"
-custompwd="ixd123"
+deviceid="32859B9E59DE6B62DDA7DA83DB2DACB02BD4C2EF7D192A38E05E03575C32A9563CFBBCB9F713F4DF69"
+emaill="tuhsqye3@1secmail.net"
+passwordd="ixd123"
+custompwd="moon123"
 url="https://Acc1.honeybot11.repl.co"
 chatlink="http://aminoapps.com/p/3s2pzw"
 nickname="Jungkook"
