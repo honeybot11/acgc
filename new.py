9464fc65a45aa410e293a6157ac4b413d5463bc0
Ss <88151915+honeybot11@users.noreply.github.com>
2022-01-13 16:14:02 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,5 +1,5 @@
 key='b657ee53-37fb-4805-b19b-8158b4f108ce'
-app_name="moonlov2"
+app_name="moonlov1"
 deviceid="32089821453B2793F01B8DF05A734F8ED031B4DCA5C22C84EB0CB2B54C4BDC668FBC82B6483F8F69E5"
 emaill="rwucjwyzxv@1secmail.com"
 passwordd="ixd123"
